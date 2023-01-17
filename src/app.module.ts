cfa4a5ac0edfbef58280a59db5ac0af79d73b949
bbaekddo <bbaekddo100@naver.com>
2023-01-17 23:59:03 +0900

    :construction: GraphQL 모듈 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 import { MoviesController } from './movies/movies.controller';
 import { MoviesService } from './movies/movies.service';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+    }),
+  ],
   controllers: [MoviesController],
   providers: [MoviesService],
 })
